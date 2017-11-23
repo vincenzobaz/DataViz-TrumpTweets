daea94f49a7fc6fdf6315be59eeffca4ae467e15
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-11-23 15:46:38 +0100

    Try to visualize histograms after bubbles. Element is appended but overlays with bubbles.

--- webapp/app/bubbles.js
+++ webapp/app/bubbles.js
@@ -1,16 +1,16 @@
 import * as d3 from 'd3';
 import * as _ from 'lodash';
 
-export class Bubbles {
+class Bubbles {
     constructor(anchor, bubbles, dimensions, animTime = 1000) {
         this.anchor = anchor;
         const layout = d3.pack()
-            .size(dimensions)
-            .padding(1.5);
+                         .size(dimensions)
+                         .padding(5);
 
         const root = d3.hierarchy({ children: bubbles })
-            .sum(bubble => bubble.size)
-            .sort((a, b) => b.size - a.size);
+                       .sum(bubble => bubble.size)
+                       .sort((a, b) => b.size - a.size);
 
         layout(root);
         this.bubbles = root.children;
@@ -21,22 +21,22 @@ export class Bubbles {
         const color = d3.scaleOrdinal(d3.schemeCategory10);
 
         const bubbleGroup = this.anchor.append('g')
-            .classed('bubbles', true);
+                                       .classed('bubbles', true);
         const start = bubbleGroup.selectAll('g')
-            .data(this.bubbles)
-            .enter()
-            .append('g')
-            .attr('transform', layout_bubble => `translate(${layout_bubble.x}, ${layout_bubble.y})`);
+                                 .data(this.bubbles)
+                                 .enter()
+                                 .append('g')
+                                 .attr('transform', layout_bubble => `translate(${layout_bubble.x}, ${layout_bubble.y})`);
 
         start.append('circle')
-            .attr('fill', b => color(b.data.size))
-            .attr('r', b => b.r)
-            .on('click', this.callback.bind(this));
+             .attr('fill', b => color(b.data.size))
+             .attr('r', b => b.r)
+             .on('click', this.callback.bind(this));
 
         start.append('text')
-            .attr('text-anchor', 'middle')
-            .attr('alignment-baseline', 'middle')
-            .text(b => b.data.text);
+             .attr('text-anchor', 'middle')
+             .attr('alignment-baseline', 'middle')
+             .text(b => _.capitalize(b.data.text.replace('_', ' ')));
     }
 
     callback(layout_bubble) {
@@ -59,12 +59,14 @@ export class Bubbles {
             .selectAll('g')
             .transition()
             .attr('transform', layout_bubble => `translate(${layout_bubble.x}, ${layout_bubble.y})`)
+            .selectAll('circle')
+            .attr('r', b => b.r)
             .duration(this.animTime);
         return this;
     }
 
     flattenBubbles() {
-        const padding = 2;
+        const padding = 10;
         const padCount = this.bubbles.length - 1;
         const totLength = this.bubbles.reduce((acc, n) => acc + n.data.size, 0); + padding * padCount;
         const scale = len => (this.dimensions[0] * len) / totLength;
@@ -85,15 +87,20 @@ export class Bubbles {
         };
 
         this.anchor.select('.bubbles')
-            .selectAll('g')
-            .transition()
-            .attr('transform', (n, i) => translate(n, i))
-            .selectAll('circle')
-            .attr('r', n => {
-                const [start, end] = nameToPos[n.data.text];
-                return (end - start) / 2
-            })
-            .duration(this.animTime);
+                   .selectAll('g')
+                   .transition()
+                   .attr('transform', (n, i) => translate(n, i))
+                   .selectAll('circle')
+                   .attr('r', n => {
+                       const [start, end] = nameToPos[n.data.text];
+                       return (end - start) / 2
+                   })
+                   .duration(this.animTime);
         return this;
     }
 }   
+
+export function generateBubblePlot(anchor, bubbles, dimensions, animTime = 1000) {
+    const bubble = new Bubbles(anchor, bubbles, dimensions, animTime = 1000);
+    return anchor.select('.bubbles')
+}
--- webapp/app/main.js
+++ webapp/app/main.js
@@ -1,7 +1,8 @@
 import * as _ from 'lodash';
 import * as d3 from 'd3';
+import * as c3 from 'c3';
 
-import { Bubbles } from './bubbles';
+import { generateBubblePlot } from './bubbles';
 
 const server_url = 'http://localhost:8000/';
 
@@ -11,16 +12,57 @@ const vizDiv = document.getElementById('viz');
 const chartWidth = vizDiv.clientWidth;
 const chartHeight = vizDiv.clientHeight;
 
-const svg = d3.select('#viz')
-              .append('svg')
-              .attr('preserveAspectRatio', 'xMinYMin meet')
-              .attr('viewBox', `0 0 ${chartWidth} ${chartHeight}`)
-              .classed('svg-content-responsive', true);
+const bigSVG = d3.select('#viz')
+                 .append('svg')
+                 .attr('id', 'global-container')
+                 .attr('preserveAspectRatio', 'xMinYMin meet')
+                 .attr('viewBox', `0 0 ${chartWidth} ${chartHeight}`)
+                 .classed('svg-content-responsive', true)
+
+function splitWords(text) {
+  return text.split(' ');
+}
+
+function focusOnTopic(topicName, tweets, anchor) {
+  console.log(_.upperCase(topicName));
+  const words = _.flatMap(tweets, tweet => splitWords(tweet.text))
+  const sortedCounts = _.sortBy(_.toPairs(_.countBy(words)), tup => -tup[1])
+  const [topWords, topCounts] = _.unzip(sortedCounts.slice(0, 50))
+
+  anchor.append('svg')
+        .attr('id', 'hists')
+  c3.generate({
+    bindto: '#hists',
+    data: {
+      columns: ['counts', ...topCounts],
+      axes: {
+        counts: 'c'
+      },
+      types: {
+        counts: 'bar'
+    },
+   }
+  })
+}
+
+function topicsViz(topics) {
+  const topicBubbleSVG = bigSVG.append('svg')
+                               .attr('id', 'topic-bubbles-container')
+                               .attr('width', '50%')
+                               .attr('preserveAspectRatio', 'xMinYMin meet')
+                               .attr('viewBox', `0 0 ${chartWidth} ${chartHeight}` )
+
+  const bubbles = topics.map(tuple => ({
+    text: tuple[0],
+    size: tuple[1].length,
+    callback: () => focusOnTopic(tuple[0], tuple[1], topicBubbleSVG)
+  }));
+
+  const bubblePlot = generateBubblePlot(topicBubbleSVG, bubbles, [chartWidth, chartHeight]);
+
+}
 
 // Create topic bubble chart
 fetch(server_url + 'topics.json')
   .then(el => el.json())
-  .then(data => {
-    const bubbles = data.map(tuple => ({text: tuple[0], size: tuple[1].length, callback: () => console.log('Clicked on ' + tuple[0])}));
-    const bubblePlot = new Bubbles(svg, bubbles, [1280, 720]);
-  })
+  .then(topicsViz);
