ae4fec565b99531d2a9ed87797b511ebd40cb871
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-11-24 22:56:52 +0100

    Switch to vertical layout and clean

--- webapp/app/bubbles.js
+++ webapp/app/bubbles.js
@@ -1,106 +1,111 @@
 import * as d3 from 'd3';
 import * as _ from 'lodash';
 
-class Bubbles {
-    constructor(anchor, bubbles, dimensions, animTime = 1000) {
-        this.anchor = anchor;
+export class Bubbles {
+    constructor(selector, bubbles, dimensionsFull, dimensionsCollapsed, animTime = 1000) {
         const layout = d3.pack()
-                         .size(dimensions)
-                         .padding(5);
+            .size(dimensionsFull)
+            .padding(5);
 
         const root = d3.hierarchy({ children: bubbles })
-                       .sum(bubble => bubble.size)
-                       .sort((a, b) => b.size - a.size);
+            .sum(bubble => bubble.size)
+            .sort((a, b) => b.size - a.size);
 
         layout(root);
         this.bubbles = root.children;
         this.selectedBubble = null;
-        this.dimensions = dimensions;
+        this.dimensionsFull = dimensionsFull;
+        this.dimensionsCollapsed = dimensionsCollapsed;
         this.animTime = animTime;
-
-        const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-        const bubbleGroup = this.anchor.append('g')
-                                       .classed('bubbles', true);
-        const start = bubbleGroup.selectAll('g')
-                                 .data(this.bubbles)
-                                 .enter()
-                                 .append('g')
-                                 .attr('transform', layout_bubble => `translate(${layout_bubble.x}, ${layout_bubble.y})`);
-
-        start.append('circle')
-             .attr('fill', b => color(b.data.size))
-             .attr('r', b => b.r)
-             .on('click', this.callback.bind(this));
-
-        start.append('text')
-             .attr('text-anchor', 'middle')
-             .attr('alignment-baseline', 'middle')
-             .text(b => _.capitalize(b.data.text.replace('_', ' ')));
+        this.selector = selector;
+        this.svg = d3.select(this.selector)
+            .append('svg')
+            .attr('id', 'svg-bubbles')
+            .attr('viewBox', `0 0 ${dimensionsFull[0]} ${dimensionsFull[1]}`)
+            .attr('width', dimensionsFull[0])
+            .attr('height', dimensionsFull[1]);
+        this.drawn = false;
     }
 
     callback(layout_bubble) {
-        if (!this.selectedBubble) {
-            this.flattenBubbles();
+        // click on already selected => reset
+        if (this.selectedBubble && layout_bubble.data.text === this.selectedBubble) {
+            this.selectedBubble = null;
+            this.drawFull();
+        } else { // otherwise select new bubble and redraw
             this.selectedBubble = layout_bubble.data.text;
+            this.drawCollapsed();
             layout_bubble.data.callback();
-        } else {
-            if (layout_bubble.data.text === this.selectedBubble) {
-                this.selectedBubble = null;
-                this.spreadBubbles();
-            } else {
-                this.selectedBubble = layout_bubble.data.text;
-            }
         }
     }
 
-    spreadBubbles() {
-        this.anchor.select('.bubbles')
-            .selectAll('g')
-            .transition()
-            .attr('transform', layout_bubble => `translate(${layout_bubble.x}, ${layout_bubble.y})`)
-            .selectAll('circle')
-            .attr('r', b => b.r)
-            .duration(this.animTime);
-        return this;
+    drawFull() {
+        if (!this.drawn) {
+            const [width, height] = this.dimensionsFull;
+            const bubbleGroup = this.svg.append('g')
+                .classed('bubbles', true);
+
+            const start = bubbleGroup.selectAll('g')
+                .data(this.bubbles)
+                .enter()
+                .append('g')
+                .attr('transform', layout_bubble => `translate(${layout_bubble.x}, ${layout_bubble.y})`)
+
+            const color = d3.scaleOrdinal(d3.schemeCategory10);
+            start.append('circle')
+                .attr('fill', b => color(b.data.size))
+                .attr('r', b => b.r)
+                .on('click', this.callback.bind(this));
+
+            start.append('text')
+                .attr('text-anchor', 'middle')
+                .attr('alignment-baseline', 'middle')
+                .text(b => _.capitalize(b.data.text.replace('_', ' ')));
+            this.drawn = true;
+        } else {
+            this.svg.select('.bubbles')
+                .selectAll('g')
+                .transition()
+                .attr('transform', d => `translate(${d.x}, ${d.y})`)
+                .selectAll('circle')
+                .attr('r', d => d.r)
+                .duration(this.animTime);
+        }
     }
 
-    flattenBubbles() {
+
+    drawCollapsed() {
         const padding = 10;
         const padCount = this.bubbles.length - 1;
-        const totLength = this.bubbles.reduce((acc, n) => acc + n.data.size, 0); + padding * padCount;
-        const scale = len => (this.dimensions[0] * len) / totLength;
-        const newSizes = this.bubbles.map(b => scale(b.data.size)); //[new_size]
+        const ratio = 5 / 3;
+        const normalSize = this.dimensionsCollapsed[1] / (this.bubbles.length - 1 + ratio);
+        const specialSize = normalSize * ratio;
+        const newSizes = this.bubbles.map(b => b.data.text === this.selectedBubble ? specialSize : normalSize)
         for (let i = 1; i < newSizes.length; ++i) {
             newSizes[i] += newSizes[i - 1];
         }
-
         const startToEnd = _.zip([0, ...newSizes.slice(0, -1)], newSizes);
         const nameToPos = _.fromPairs(_.zip(this.bubbles.map(b => b.data.text), startToEnd));
-        const y = _.max(startToEnd.map(t => (t[1] - t[0]) / 2));
+
+        const x = _.max(startToEnd.map(t => (t[1] - t[0]) / 2));
 
         const translate = (node, index) => {
             const [start, end] = nameToPos[node.data.text];
-            let x = start + (end - start) / 2;
-            x = index > 0 ? x + padding : x;
+            let y = start + (end - start) / 2;
+            y = index > 0 ? y + padding : y;
             return `translate(${x}, ${y})`;
         };
 
-        this.anchor.select('.bubbles')
-                   .selectAll('g')
-                   .transition()
-                   .attr('transform', (n, i) => translate(n, i))
-                   .selectAll('circle')
-                   .attr('r', n => {
-                       const [start, end] = nameToPos[n.data.text];
-                       return (end - start) / 2
-                   })
-                   .duration(this.animTime);
-        return this;
+        d3.select(this.selector)
+            .select('.bubbles')
+            .selectAll('g')
+            .transition()
+            .attr('transform', (n, i) => translate(n, i))
+            .selectAll('circle')
+            .attr('r', n => {
+                const [start, end] = nameToPos[n.data.text];
+                return (end - start) / 2
+            })
+            .duration(this.animTime);
     }
-}   
-
-export function generateBubblePlot(anchor, bubbles, dimensions, animTime = 1000) {
-    const bubble = new Bubbles(anchor, bubbles, dimensions, animTime = 1000);
-    return anchor.select('.bubbles')
 }
