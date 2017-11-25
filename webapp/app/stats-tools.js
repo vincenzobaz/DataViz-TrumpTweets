972c9cbd7bb597b79728900b290ddaf38588cc8a
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-11-25 17:53:08 +0100

    Implement timeseries creation

--- webapp/app/stats-tools.js
+++ webapp/app/stats-tools.js
@@ -7,11 +7,11 @@ function breakText(text) {
     return noPunct.split(' ');
 }
 
-export function getRetweetCounts(tweets) {
+export function getRetweetCount(tweets) {
     return _.sumBy(tweets, 'retweet_count');
 }
 
-export function getLikeCounts(tweets) {
+export function getLikeCount(tweets) {
     return _.sumBy(tweets, 'favorite_count');
 }
 
@@ -25,4 +25,27 @@ export function getWordUsage(tweets, getCoefficient = t => 1) {
     }
 
     return res;
+}
+
+export function createTimeSeries(tweets, getCoefficient = t => 1) {
+    const monthYearVal = _.map(tweets, tweet => {
+        const date = new Date(tweet['created_at']);
+        return {
+            yearMonth: date.getFullYear() + ' ' + date.getMonth(),
+            value: getCoefficient(tweet)
+        }
+    });
+
+    const yearMonthSum = {};
+    for (let tup of monthYearVal) {
+        yearMonthSum[tup.yearMonth] = _.get(yearMonthSum, tup.yearMonth, 0) + tup.value;
+    }
+
+    const cleanStrings = _.map(_.toPairs(yearMonthSum), yearMonthVal => {
+        const [yearMonth, val] = yearMonthVal;
+        const [year, month] = yearMonth.split(' ');
+        const d = Date(year, month, 1, 0, 0, 0, 0);
+        return [d, val];
+    });
+    return _.unzip(cleanStrings);
 }
