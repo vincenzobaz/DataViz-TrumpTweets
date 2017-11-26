764fe204c995e7707f15db0f46c56eb3eef7e87e
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-11-26 19:11:26 +0100

    Add pretty welcome page

--- webapp/app/main.js
+++ webapp/app/main.js
@@ -1,7 +1,26 @@
 import { dashboard } from './dashboard';
+import * as d3 from 'd3';
 
 require('./main.scss'); // will build CSS from SASS 3 file
 
-fetch(dataUrl)
-  .then(el => el.json())
-  .then(dashboard);
+const createDivs = () => {
+  const container = d3.select('body')
+                      .append('div')
+                      .attr('id', 'container');
+  container.append('div')
+           .attr('id', 'sidebar');
+  container.append('div')
+           .attr('id', 'content-pane')
+           .append('div')
+           .attr('id', 'flattened-bubbles');
+};
+
+const start = () => {
+  d3.select('#start-button').node().disabled = true;
+  d3.select('#intro').append('p').html('Loading data...');
+  d3.select('#intro').remove();
+  createDivs();
+  fetch(dataUrl).then(el => el.json()).then(dashboard);
+};
+
+document.getElementById('start-button').addEventListener('click', start);
