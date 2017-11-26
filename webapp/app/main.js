7c5c5b689f04cc1597901e416e1aa03cd09a5a62
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-11-26 19:29:02 +0100

    Reorder instructions to have desired output

--- webapp/app/main.js
+++ webapp/app/main.js
@@ -5,22 +5,24 @@ require('./main.scss'); // will build CSS from SASS 3 file
 
 const createDivs = () => {
   const container = d3.select('body')
-                      .append('div')
-                      .attr('id', 'container');
+    .append('div')
+    .attr('id', 'container');
   container.append('div')
-           .attr('id', 'sidebar');
+    .attr('id', 'sidebar');
   container.append('div')
-           .attr('id', 'content-pane')
-           .append('div')
-           .attr('id', 'flattened-bubbles');
+    .attr('id', 'content-pane')
+    .append('div')
+    .attr('id', 'flattened-bubbles');
 };
 
 const start = () => {
   d3.select('#start-button').node().disabled = true;
   d3.select('#intro').append('p').html('Loading data...');
-  d3.select('#intro').remove();
-  createDivs();
-  fetch(dataUrl).then(el => el.json()).then(dashboard);
+  fetch(dataUrl).then(el => el.json()).then(data => {
+    d3.select('#intro').remove();
+    createDivs();
+    dashboard(data);
+  });
 };
 
 document.getElementById('start-button').addEventListener('click', start);
