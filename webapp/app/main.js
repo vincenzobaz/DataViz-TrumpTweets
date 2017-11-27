35a3b36a790644974fd3e96021e3fbf03a45a8d7
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-11-27 17:50:49 +0100

    Scroll to app instead of deleting intro

--- webapp/app/main.js
+++ webapp/app/main.js
@@ -19,8 +19,9 @@ const start = () => {
   d3.select('#start-button').node().disabled = true;
   d3.select('#intro').append('p').html('Loading data...');
   fetch(dataUrl).then(el => el.json()).then(data => {
-    d3.select('#intro').remove();
+    d3.select('#intro').select('p').remove();
     createDivs();
+    scrollTo(0, document.getElementById('container').offsetTop);
     dashboard(data);
   });
 };
