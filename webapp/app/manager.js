ea3e16bd628c593d2e1d837ab591af346845e805
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-12-22 16:42:59 +0100

    Document class

--- webapp/app/manager.js
+++ webapp/app/manager.js
@@ -3,12 +3,16 @@ import { Bubbles } from './bubbles';
 import { VisualizerStacker } from './visualizer-stacker';
 
 
+/**
+ * Super class managing bubbles and Visualizers.
+ */
 export class Manager {
     constructor(selector, id) {
         this.focusedOn = null;
         this.selector = selector;
         this.id = id;
 
+        // Create div
         d3.select(selector)
             .append('div')
             .attr('id', id)
@@ -37,15 +41,22 @@ export class Manager {
         this.divExists = false;
     }
 
+    /**
+     * Callback executed when a bubble is clicked on or when entering multiple bubbles comparison.
+     * @param newFocus can be an array (if comparing multiple bubbles) or a string indicating
+     *  the bubble being analyzed
+     */
     focusOn(newFocus) {
         // Nothing is selected, create viz
         if (Array.isArray(newFocus)) {
-            if (newFocus.length > 0) {
-                if (this.focusedOn) this.visualizer.hide();
+            // If arg is array, it is the list of bubbles to compare
+            if (newFocus.length > 0) { // List not empty => enter comparison
+                if (this.focusedOn) this.visualizer.hide(); // Hide previous visualizer
+                // Obain specialized stacked visualizer
                 this.visualizer = new VisualizerStacker(newFocus.map(n => this.getVisualizer(n)));
                 this.visualizer.draw();
                 this.focusedOn = 'multiple';
-            } else {
+            } else { // List empty => reset
                 this.visualizer && this.visualizer.hide();
                 this.visualizer = null;
                 this.focusedOn = null;
@@ -53,11 +64,12 @@ export class Manager {
                 this.draw();
             }
         } else {
-            if (this.focusedOn == newFocus) {
+            // Otherwise it is a bubble name
+            if (this.focusedOn == newFocus) { // Same name as before => reset
                 this.visualizer.hide();
                 this.visualizer = null;
                 this.focusedOn = null;
-            } else {
+            } else { // New name => refocus
                 if (this.focusedOn) this.visualizer.hide();
                 this.visualizer = this.getVisualizer(newFocus);
                 this.visualizer.draw();
