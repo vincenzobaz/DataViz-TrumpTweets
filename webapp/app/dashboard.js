5d0fa461823a7689c531d2e544553393d184be8c
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-12-22 14:22:59 +0100

    Document class

--- webapp/app/dashboard.js
+++ webapp/app/dashboard.js
@@ -6,10 +6,14 @@ import { Bubbles } from './bubbles';
 import { TopicsManager } from './topics-manager';
 import { SentimentsManager } from './sentiments-manager';
 
+/**
+ * Entrypoint of the application, receives data and handles sidebar and maanagers
+ */
 class Dashboard {
     constructor(tweets) {
         this.tweets = _.filter(tweets, t => !t['is_retweet']);
         this.displayed = null;
+        // Create both manager, sidebar will decide which one is shown
         this.topicsManager = new TopicsManager('#content-pane', tweets);
         this.sentimentsManager = new SentimentsManager('#content-pane', tweets);
 
@@ -25,16 +29,22 @@ class Dashboard {
         const sidebarWidth = document.getElementById('sidebar').clientWidth;
         const sidebarHeight = document.getElementById('sidebar').clientHeight;
 
+        // Create sidebar
         const sidebar = new Sidebar('#sidebar', panes, [sidebarWidth, sidebarHeight]);
     }
 
-    display(what) {
-        if (what != this.displayed) {
-            this.displayed = what;
-            if (what == 'topics') {
+    /**
+     * Callback called upon click on a sidebar element.
+     * @param {string} sidebarElement name of the element clicked
+     */
+    display(sidebarElement) {
+        if (sidebarElement != this.displayed) { // If clicked is different from previously selected
+            this.displayed = sidebarElement;
+            // Instantiate appropriate manager.
+            if (sidebarElement == 'topics') {
                 this.sentimentsManager.hide();
                 this.topicsManager.draw();
-            } else if (what == 'sentiments') {
+            } else if (sidebarElement == 'sentiments') {
                 this.topicsManager.hide();
                 this.sentimentsManager.draw();
             }
